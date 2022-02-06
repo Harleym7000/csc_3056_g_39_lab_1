c950543f94918db837372dc0af9cff1888fdd09c
Harleym7000 <harleym7000@gmail.com>
2022-02-06 19:08:07 +0000

    Initial commit

---
+++ src/tests/TransactionTests.java
@@ -0,0 +1,16 @@
+package tests;
+
+import java.sql.Date;
+
+import model.Transaction;
+
+public class TransactionTests {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Transaction t1 = new Transaction());
+
+	}
+
+}
